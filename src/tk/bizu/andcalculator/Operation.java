31c5bf9580e50ce6d12408fee0d60a95b17e10d6
sysilion <sysilion+github@gmail.com>
2015-04-02 16:54:29 +0900

    fix brace, minus result problem

--- src/tk/bizu/andcalculator/Operation.java
+++ src/tk/bizu/andcalculator/Operation.java
@@ -8,10 +8,23 @@ import android.util.Log;
 public class Operation {
 	protected String Calc(String str){
 		if(str.indexOf('(') != -1){
-			int fs = str.indexOf('(');
-			int ls = str.lastIndexOf(')');
-			String s = Calc(str.substring(fs + 1, ls));
-			str = str.substring(0, fs) + s + str.substring(ls+1, str.length());
+			int count = 0;
+			int fs = 0, ls = 0;
+			for (int i = 0; i < str.length(); i++){
+				if (str.charAt(i) == '(') {
+					if (count == 0){
+						fs = i;
+					}
+					count++;
+				} else if (str.charAt(i) == ')'){
+					count--;
+					if (count == 0){
+						ls = i;
+						String s = Calc(str.substring(fs + 1, ls));
+						str = str.substring(0, fs) + s + str.substring(ls+1, str.length());
+					}
+				}
+			}
 		}
 		
 		double cnt = 0;
@@ -21,10 +34,15 @@ public class Operation {
 		
 		Stk_Num.push(Double.parseDouble(ST_Num.nextToken()));
 
-		while(ST_Num.hasMoreTokens()){
+		while(ST_Num.hasMoreTokens() || ST_Oper.hasMoreTokens()){
 			
 			char oper = ST_Oper.nextToken().charAt(0);
-			String num = ST_Num.nextToken();
+			String num = "";
+			if (ST_Num.hasMoreTokens()){
+				num = ST_Num.nextToken();
+			} else {
+				num = ""+Stk_Num.pop();
+			}
 			double a;
 			
 			if(oper == '*'){
